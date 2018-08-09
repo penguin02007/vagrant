9028eec225da04acd65157fefcc8304faa112def
Leo Chan <ka.chan@rakops.com>
2018-08-09 12:23:14 +0200

    reorg vagrant puppet provisioner

--- puppet/modules/docker/manifests/init.pp
+++ puppet/modules/docker/manifests/init.pp
@@ -1,11 +1,13 @@
-package { 'docker.io':
-  ensure => latest,
-}
-package { 'docker-compose':
-  ensure  => latest,
-  require => Package['docker.io'],
-}
-service { 'docker':
-  ensure  => running,
-  require => Package['docker.io'],
+class docker {
+  package { 'docker.io':
+    ensure => latest,
+  }
+  package { 'docker-compose':
+    ensure  => latest,
+    require => Package['docker.io'],
+  }
+  service { 'docker':
+    ensure  => running,
+    require => Package['docker.io'],
+  }
 }
